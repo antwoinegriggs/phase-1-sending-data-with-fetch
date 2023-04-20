35a35f2c9bef3f3230223168f4500f412636b79d
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-04-20 04:55:37 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  let data = {
+    name: userName,
+    email: userEmail,
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      document.body.innerHTML = `${data.id}`;
+    })
+    .catch((error) => {
+      console.log(error.message);
+      document.body.innerHTML = error.message;
+    });
+}
